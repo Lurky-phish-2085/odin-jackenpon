9501fe14c37a72f29b71d4520f453710a661b974
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-09-10 17:40:30 +0800

    Implement single round game logic

--- jakenpon.js
+++ jakenpon.js
@@ -8,13 +8,31 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const promptMessage = 'Choose which to use (rock, paper, scissors): '
+  const promptMessage = 'Choose which to use (rock, paper, scissors): ';
   let humanChoice = prompt(promptMessage);
 
   while (!choices.includes(humanChoice.toLowerCase())) {
-    alert('Please try again.')
+    alert('Please try again.');
     humanChoice = prompt(promptMessage);
   }
 
   return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    return;
+  }
+
+  const playerWon = (humanChoice === 'rock' && computerChoice === 'scissors') || 
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper');
+
+  if (!playerWon) {
+    console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    return;
+  }
+
+  console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
 }
