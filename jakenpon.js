6998a345e7a8b76c1a87c08ecdf9498d5b3496ab
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-09-10 17:21:58 +0800

    Declare player and computer score variables

--- jakenpon.js
+++ jakenpon.js
@@ -1,5 +1,8 @@
 const choices = ['rock', 'paper', 'scissors'];
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
