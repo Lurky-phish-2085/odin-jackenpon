97dfe5a158cf79e6426e0e4fffea0d45418f4361
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-09-10 17:21:02 +0800

    Implement player choice logic

--- jakenpon.js
+++ jakenpon.js
@@ -3,3 +3,15 @@ const choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getHumanChoice() {
+  const promptMessage = 'Choose which to use (rock, paper, scissors): '
+  let humanChoice = prompt(promptMessage);
+
+  while (!choices.includes(humanChoice.toLowerCase())) {
+    alert('Please try again.')
+    humanChoice = prompt(promptMessage);
+  }
+
+  return humanChoice;
+}
